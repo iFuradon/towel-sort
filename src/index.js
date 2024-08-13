66dd07b6c77f02ae02770661b76ca6bd24e08ad2
iFuradon <iFuradon@gmail.com>
2024-08-14 00:36:28 +0400

    fix: errors

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 
-function towelSort(matrix) {
+/*function towelSort(matrix) {
+  
+  if (!Array.isArray(matrix) || matrix.length === 0) {
+    return [];
+  }
 
   return matrix.reduce((acc, row, index) => {
-    
     if (index % 2 !== 0) {
         row.reverse();
     }
-
     return acc.concat(row);
-
-}, []);
-
+  }, []);
 }
 
 module.exports = function towelSort (matrix) {
   return [];
+}*/
+module.exports = function towelSort (matrix) {
+  if (!Array.isArray(matrix) || matrix.length === 0) {
+    return [];
+  }
+
+  return matrix.reduce((acc, row, index) => {
+    if (index % 2 !== 0) {
+        row.reverse();
+    }
+    return acc.concat(row);
+  }, []);
 }
